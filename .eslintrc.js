9e2c1a0c67366b05b9042ff09784a828efe728b1
unknown <reza.erami@snapp.market>
2020-07-01 15:05:18 +0430

    fix eslint
    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,31 @@ const fs = require('fs');
 const prettierOptions = JSON.parse(fs.readFileSync('./.prettierrc', 'utf8'));
 
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": ["prettier"],
-  "ignorePatterns": ["*.test.js","*.spec.js", "**/dist/*",  "**/node_modules/*"],
-  "plugins": ["prettier"],
-  "rules": {
-      "prettier/prettier": ["error", prettierOptions]
-    }
+  'parser': 'babel-eslint',
+  'extends': ['prettier'],
+  'ignorePatterns': ['*.test.js','*.spec.js', '**/dist/*',  '**/node_modules/*'],
+  'plugins': ['prettier'],
+  'rules': {
+    'prettier/prettier': ['error', prettierOptions],
+    'class-methods-use-this': 0,
+    'import/imports-first': 0,
+    'import/newline-after-import': 0,
+    'import/no-dynamic-require': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default': 0,
+    'import/no-unresolved': 2,
+    'import/no-webpack-loader-syntax': 0,
+    'import/prefer-default-export': 0,
+    indent: [
+      2,
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'max-len': 0,
+    'newline-per-chained-call': 0,
+    'no-confusing-arrow': 0,
+    'no-console': 1,
+  }
 };

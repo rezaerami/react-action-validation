1cdf92a0f4454e80cd6c9a46817b49abcd9a30e4
Reza Erami <reza@erami.name>
2020-07-11 08:47:49 +0430

    fix validation gate and validator
    pass store and action from validation gate
    accept store and action to ba able to dispatch an action in custom
    validators
    update docs

--- src/validationGate.js
+++ src/validationGate.js
@@ -1,15 +1,15 @@
-export default validations => () => next => async action => {
+export default validations => store => next => async action => {
   if (!validations[action.type]) {
     next(action);
   } else {
     const validation = new validations[action.type]();
     try {
       await validation.validate(action.payload);
-      validation.accept(action.type);
+      validation.accept(store)(action);
       next(action);
     } catch (e) {
       console.error(e);
-      validation.reject(action.type);
+      validation.reject(store)(action);
     }
   }
 };
